91543ffa1f90bd27c9dd69410a10a314419c224c
Reem Mathbout <reem.mathbout89@gmail.com>
2026-05-21 19:33:14 +0200

    add inference time in api

--- app/api.py
+++ app/api.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-# from typing import Dict, Any, List
 import pandas as pd
 import joblib
 from contextlib import asynccontextmanager
